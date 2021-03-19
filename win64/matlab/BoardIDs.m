eb942518093a67fd9d2cc23cf1094ecf52820dad
R Jano <rokaijano@gmail.com>
2021-03-19 08:49:32 +0100

    Matlab sources added

---
+++ win64/matlab/BoardIDs.m
@@ -0,0 +1,25 @@
+classdef BoardIDs < int32
+    enumeration
+        PLAYBACK_FILE_BOARD (-3)
+        STREAMING_BOARD (-2)
+        SYNTHETIC_BOARD (-1)
+        CYTON_BOARD (0)
+        GANGLION_BOARD (1)
+        CYTON_DAISY_BOARD (2)
+        NOVAXR_BOARD (3)
+        GANGLION_WIFI (4)
+        CYTON_WIFI (5)
+        CYTON_DAISY_WIFI (6)
+        BRAINBIT_BOARD (7)
+        UNICORN_BOARD (8)
+        CALLIBRI_EEG_BOARD (9)
+        CALLIBRI_EMG_BOARD (10)
+        CALLIBRI_ECG_BOARD (11)
+        FASCIA_BOARD (12)
+        NOTION_1_BOARD (13)
+        NOTION_2_BOARD (14)
+        IRONBCI_BOARD (15)    
+        MINDROVE_WIFI_BOARD(16)
+        MINDROVE_BLUETOOTH_BOARD(17)
+    end
+end
